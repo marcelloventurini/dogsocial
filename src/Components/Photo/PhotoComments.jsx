29ca93f9326d4dec733dc036a8f432d43243ab3c
Marcello Venturini <marcellofventurini@gmail.com>
2023-05-05 17:39:33 -0300

    Post comments.

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,6 +1,23 @@
-function PhotoComments() {
+import { useContext, useState } from 'react'
+import { UserContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComments.module.css'
+
+function PhotoComments(props) {
+  const [comments, setComments] = useState(() => props.comments)
+  const { login } = useContext(UserContext)
+  
   return (
-    <div>PhotoComments</div>
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            {comment.comment_author}: {comment.comment_content}
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
 
