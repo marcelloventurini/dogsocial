36f477d8afd7ed22d5a57b50dc2b3b51bfb20950
Marcello Venturini <marcellofventurini@gmail.com>
2023-05-08 17:52:04 -0300

    Create infinite scoll.

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import UserHeader from './UserHeader'
 import { Route, Routes } from 'react-router-dom'
 import Feed from '../Feed/Feed'
 import UserPhotoPost from './UserPhotoPost'
 import UserStatistics from './UserStatistics'
+import { UserContext } from '../../UserContext'
 
 function User() {
+  const { data } = useContext(UserContext)
+
   return (
     <section className='container'>
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
+        <Route path='/' element={<Feed user={data.id} />} />
         <Route path='/postar' element={<UserPhotoPost />} />
         <Route path='/estatisticas' element={<UserStatistics />} />
       </Routes>
