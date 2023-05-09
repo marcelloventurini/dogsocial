96b41eede9e1a415e92cc39bcc07556a21802019
Marcello Venturini <marcellofventurini@gmail.com>
2023-05-09 11:54:22 -0300

    Create User profile.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { UserStorage } from './UserContext'
 import User from './Components/User/User'
 import ProtectedRoute from './Components/Helper/ProtectedRoute'
 import Photo from './Components/Photo/Photo'
+import UserProfile from './Components/User/UserProfile'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
                 <User />
               </ProtectedRoute>} />
             <Route path='foto/:id' element={<Photo />} />
+            <Route path='perfil/:user' element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
